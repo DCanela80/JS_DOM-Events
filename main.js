9071962532f1798be145bcf8133d154a486e4c7c
Diana Canela <DCanela80@gmail.com>
2023-11-27 09:12:03 -0700

    completed exercises

--- main.js
+++ main.js
@@ -3,33 +3,72 @@ console.log("Script attached");
 
 // Select Node #1 and change the text to: "I used the getElementById("node1") method to access this"
 
+const node1 = document.getElementById("node1");
+
+node1.textContent = "I used the getElementById('node1') method to access this";
+
 // Select Node #2 and change the text to: "I used the getElementByClassName("node2") method to access this" */
 
+const node2 = document.getElementsByClassName("node2")[0];
+
+node2.textContent = "I used the getElementsByClassName('node2') method to access this";
+
 // Select ALL the h3 tags and change the text to: "I used the getElementByTagName("h3") method to access all of these" */
+
+const allh3s = document.getElementsByTagName("h3");
+
+for (let element of allh3s) {
+    element.textContent =
+    "I used the getElementsByTagName('h3') method to access all of these";
+}
 
 /*----------- Exercise #2: CREATING/APPENDING/INSERTING ELEMENTS/OBJECTS -----------*/
 
 // TODO: Create a paragraph element using this document.createElement() and put this text inside "This node was created using the createElement() method"
 
+const newParagraph = document.createElement("p");
+newParagraph.textContent =
+"This node was created using the createElement() method";
+
 // TODO: Append the created node to the parent node using the element.appendChild() method
 
-// TODO: Create a <a> element using this document.createElement() and put this text inside "I am a <a> tag"
+const parent = document.querySelector("#parent");
+parent.appendChild(newParagraph);
 
+// TODO: Create a <a> element using this document.createElement() and put this text inside "I am a <a> tag"
 // BONUS: Add a link href to the <a>
 
+const link = document.createElement("a");
+link.textContent = "I am an <a> tag";
+link.href = "https://www.google.com/";
+link.target = "_blank";
+
 // TODO: Insert the created <a> in the parent but before the <p> you just created using the element.insertBefore() method
+
+parent.insertBefore(link, newParagraph);
 
 /*----------- Exercise #3: REMOVING/REPLACING ELEMENTS/OBJECTS -----------*/
 
 // TODO: Replace the "Child Node" with a new <p> element that reads "New Child Node"
 
+const exercise3Container = document.querySelector("#exercise-container3");
+const oldChild = document.querySelector("#N1");
+
+const newChildNode = document.createElement("p");
+newChildNode.textContent = "New Child Node";
+
+exercise3Container.replaceChild(newChildNode, oldChild);
+
 // TODO: Remove the "New Child Node"
+setTimeout(() =>{
+    newChildNode.remove();    
+}, 2000);
+
 
 /*----------- Exercise #4: LIST ITEMS ----------- */
 // Use the following array of values to generate a list on the DOM
 
 let list = [ "apples", "bananas", "carrots", "dragon fruit", "eggplant", "fish", "grapes", "honey", "ice bag", "juice (any kind)" ];
-
 
 // TODO: Create an unordered list element
 
@@ -39,9 +78,32 @@ let list = [ "apples", "bananas", "carrots", "dragon fruit", "eggplant", "fish",
 
 // TODO: Append the unordered list to the `div#container` under exercise 4 
 
+//Create the element
+const newUL = document.createElement("ul");
+//Modify the element
+//Append the element
+document.getElementById("container").append(newUL);
+
+const newLIArray = list.map((element, index, array) => {
+    const newLI = document.createElement("li");
+    newLI.textContent = element;
+    return newLI;
+});
+newUL.append(...newLIArray);
+
+
 /*----------- Exercise #5: DOM EVENTS --------------*/
 
 // TODO: write a function called "show" which creates a new div with an alerting message to the user with this message
 // -> "Clicking the button triggers the onclick event, which calls the JS function show()... which alerts the user"
 // This div should be a 'modal' that covers the main content on the screen
 // BONUS: The modal popup should be able to be closed. Refactor for this functionality
+
+function show() {
+    //Create a new div
+    const newDiv = document.createElement("div");
+    //Modify
+    newDiv.textContent = "Clicking the button triggers the onclick event, which calls the JS function show()... which alerts the user";
+    //Append the new div
+    document.querySelector(".exercise5").append(newDiv);
+}
